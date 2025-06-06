72c91152c989bf44e105be7db93ed73311edf23e
EstrellaBM <brestrella555@gmail.com>
2025-06-06 11:07:35 -0600

    cambio1

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Mi Primera App!</Text>
-      <StatusBar style="auto" />
+      <Button
+        title="Presioname"
+        onPress={() => {
+          alert("Hola desde React Native!");
+        }}
+      />
     </View>
   );
 }
